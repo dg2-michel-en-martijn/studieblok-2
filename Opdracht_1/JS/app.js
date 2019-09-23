b12f99d7151ee4a89392020fd63b454af450ebc3
Martijn <127121@student.drenthecollege.nl>
2019-09-23 09:58:02 +0200

    waardes worden displayed.

--- Opdracht_1/JS/app.js
+++ Opdracht_1/JS/app.js
@@ -5,3 +5,8 @@ const huis = {
   plaats: "Assen",
   soortWoning: "vrijstaand huis"
 };
+
+document.getElementById('inhoud').innerHTML = (huis.inhoud);
+document.getElementById('oppervlakte').innerHTML = (huis.oppervlakte);
+document.getElementById('plaats').innerHTML = (huis.plaats);
+document.getElementById('soortWoning').innerHTML = (huis.soortWoning);
