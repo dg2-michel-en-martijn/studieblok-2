39712924b40af6e6cd02180cdb94b8540806fa0d
Martijn <127121@student.drenthecollege.nl>
2019-09-26 09:25:31 +0200

    Opdracht 2 studieblok 2 (VERGETEN IN GIT TE ZETTEN)

--- Opdracht_2/JS/app.js
+++ Opdracht_2/JS/app.js
@@ -1,5 +1,13 @@
 /* Schrijf javascript hier */
 var zin = "applicatie ontwikkelaar";
 
-
-document.getElementById('zin').innerHTML = zin.length;
+document.getElementById('len').innerHTML = "String lengte: " + zin.length;
+document.getElementById('index').innerHTML = "Locatie van 'ontwikkelaar': " + zin.indexOf("ontwikkelaar");
+document.getElementById('checkfor').innerHTML = zin.search("wikkelaar");
+document.getElementById('slice').innerHTML = zin.slice(14,17)
+function Functie() {
+  var str = document.getElementById('progr').innerHTML;
+  var replace = str.replace("Ontwikkelaar", "Programmeur");
+  document.getElementById('progr').innerHTML = replace;
+}
+document.getElementById('app').innerHTML = zin.toUpperCase();
