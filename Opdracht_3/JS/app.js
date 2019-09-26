5e85fc0ebc2c2ec3b8afa5756f687f5d7c776c90
Martijn <127121@student.drenthecollege.nl>
2019-09-26 09:27:29 +0200

    opdracht 3 studieblok 2 (GITHUB VERGETEN)

--- Opdracht_3/JS/app.js
+++ Opdracht_3/JS/app.js
@@ -1,1 +1,8 @@
 /* Schrijf javascript hier */
+var str = "opleiding";
+var lFl = "";
+for (var i = 0; i < str.length; i++) {
+  var res = str.charAt(i)
+  lFl += res + "<br>";
+}
+document.getElementById("count").innerHTML = lFl;
